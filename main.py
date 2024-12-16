6cc58b520e1550543e7590cff2f4e572cbddca21
StijnvanSteensel <schepke4@live.nl>
2024-12-16 15:01:32 +0100

    Cleaned content connected to main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
-from scrape import Scrape_website
+from scrape import (
+    Scrape_website, 
+    split_dom_content, 
+    clean_dom_content, 
+    extract_body_content,
+)
 
 st.title ("AI Web Scraper")
 url = st.text_input("Enter a website URL: ")
@@ -8,4 +13,10 @@ url = st.text_input("Enter a website URL: ")
 if st.button("Scrape site"):
     st.write("Scraping the website")
     result = Scrape_website(url)
-    print (result)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_dom_content(body_content)
+
+    st.session_state.dom_content = cleaned_content
+
+    with st.expander("View DOM content"):
+        st.text_area("DOM Content", cleaned_content, height=300)
